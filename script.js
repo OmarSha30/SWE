133e05a098cbb26ff2aa215aa151b6ca5a673a0f
OmarSha30 <94270666+OmarSha30@users.noreply.github.com>
2022-01-10 14:52:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector('.header .navbar')
+const menubutton = document.querySelector('.header .menu')
+
+menubutton.addEventListener('click', () => {
+    navbar.classList.toggle('show')
+    menubutton.classList.toggle('rotate')
+})
+
+document.onscroll = () => {
+
+    navbar.classList.remove('show')
+    menubutton.classList.remove('rotate')
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }
+    else{
+        document.querySelector('.header').classList.remove('active')
+    }
+}
+document.onload = () => {
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }
+    else{
+        document.querySelector('.header').classList.remove('active')
+    }
+}
